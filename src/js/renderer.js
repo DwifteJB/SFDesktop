a1c210b868e5911c96a79a1a66f05ada7ec5b32a
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-05-08 17:30:36 +0200

    how to define pain?????????

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -3,6 +3,31 @@ const random = (length = 8) => {
 };
 const secure_nonce = random(14)
 
-$('head').append(`<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'nonce-${secure_nonce}' https://code.jquery.com/; style-src 'self' https://*.github.io/ https://fonts.googleapis.com/; font-src 'self' https://fonts.gstatic.com/;">`);
+async function bruh() {
+    // get csp config
+    const csp_conf = await fetch("./csp.json").then(response => response.json())
+    
+    // script-src
+    const script_url = csp_conf.script_src;
+    const script_src_conf = script_url.join(' ');
+
+    // style-src
+    const style_url = csp_conf.style_src;
+    const style_src_conf = style_url.join(' ');
+
+    // font-src
+    const font_url = csp_conf.font_src;
+    const font_src_conf = font_url.join(' ');
+
+    // img-src
+    const img_url = csp_conf.img_src;
+    const img_src_conf = img_url.join(' ');
+
+    const csp_header = `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'nonce-${secure_nonce}' ${script_src_conf}; style-src 'self' 'nonce-${secure_nonce}' ${style_src_conf}; font-src 'self' ${font_src_conf}; img-src 'self' ${img_src_conf};">`;
+    $('head').append(csp_header);
+}
+bruh();
+
+// Add Urls
 $('head').append(`<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Karla:wght@200&display=swap">`)
 $('body').append(`<script nonce="${secure_nonce}">console.warn('Pasting Code in this console has an 69/10 chance of being scammed!');</script>`)
