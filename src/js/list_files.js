64f6a73a3a752234d074b208a92ae31b5aec6e71
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-05-08 22:45:43 +0200

    list folders

--- src/js/list_files.js
+++ src/js/list_files.js
@@ -1,14 +1,20 @@
 let files = "";
+let folder = "";
 
 async function loadFilesList() {
     // get Files and Key
     const LocalUserData = await fetch(window.nodeApi.gibConfigPath()).then(response => response.json())
     files = await fetch(`https://api.starfiles.co/user/files?profile=${LocalUserData.key}`).then(response => response.json())
-    // console.log(files)
+    folders = await fetch(`https://api.starfiles.co/user/folders?profile=${LocalUserData.key}`).then(response => response.json())
+    // console.log(folders)
 
     files.forEach(data => {
         // console.log(element.name)
         // Show them in index.html
         $('#filesList').append(`<a href="https://starfiles.co/file/${data.id}">${data.name}</a><br>`)
     });
+    folders.forEach(data => {
+        // Show them in index.html
+        $('#folderList').append(`<a href="https://starfiles.co/folder/${data.id}">${data.name}</a><br>`)
+    })
 }loadFilesList();
